60181b54b6979a25fe9c6d8490ef72034d51ea93
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2015-03-24 12:40:13 +0100

    Added

---
+++ src-vtiger/parent_tabdata.php
@@ -0,0 +1,14 @@
+<?php
+
+
+//This file contains the commonly used variables
+
+$parent_tab_info_array=array(1=>'My Home Page',2=>'Marketing',3=>'Sales',4=>'Support',5=>'Analytics',6=>'Inventory',7=>'Tools',8=>'Settings');
+
+
+$parent_child_tab_rel_array=array(1=>array(3,28,30,),2=>array(6,4,28,8,),3=>array(6,4,20,22,8,),4=>array(13,6,4,8,28,),5=>array(25,1,),6=>array(14,22,20,),7=>array(8,46
+,49,52,),8=>array(),);
+
+
+
+?>
