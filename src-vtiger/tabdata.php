135091f3c0d732d44f7618d04b94d217216747d2
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2015-03-24 12:43:02 +0100

    Added

---
+++ src-vtiger/tabdata.php
@@ -0,0 +1,15 @@
+<?php
+
+
+//This file contains the commonly used variables
+
+$tab_info_array=array('Dashboard'=>1,'Home'=>3,'Contacts'=>4,'Accounts'=>6,'Documents'=>8,'Emails'=>10,'HelpDesk'=>13,'Products'=>14,'Events'=>16,'Quotes'=>20,'SalesOrder'=>22,'Reports'=>25,'Webmails'=>28,'Users'=>29,'MailManager'=>30,'WSAPP'=>32,'ConfigEditor'=>33,'VtigerBackup'=>35,'Mobile'=>36,'CustomerPortal'=>38,'FieldFormulas'=>39,'Tooltip'=>45,'RecycleBin'=>46,'Webforms'=>48,'ModTracker'=>50,'Import'=>51,'Integration'=>52,'CronTasks'=>53,);
+
+$tab_seq_array=array('1'=>0,'3'=>0,'4'=>0,'6'=>0,'8'=>0,'10'=>0,'13'=>0,'14'=>0,'16'=>2,'20'=>0,'22'=>0,'25'=>0,'28'=>0,'29'=>0,'30'=>0,'32'=>0,'33'=>0,'35'=>0,'36'=>0,'38'=>0,'39'=>0,'45'=>0,'46'=>0,'48'=>0,'50'=>0,'51'=>0,'52'=>0,'53'=>0,);
+
+$tab_ownedby_array=array('1'=>1,'3'=>1,'4'=>0,'6'=>0,'8'=>0,'10'=>1,'13'=>0,'14'=>0,'16'=>0,'20'=>0,'22'=>0,'25'=>1,'28'=>1,'29'=>1,'30'=>0,'32'=>0,'33'=>0,'35'=>0,'36'=>0,'38'=>0,'39'=>0,'45'=>0,'46'=>0,'48'=>0,'50'=>0,'51'=>0,'52'=>0,'53'=>0,);
+
+$action_id_array=array('Save'=>0,'SavePriceBook'=>0,'SaveVendor'=>0,'DetailViewAjax'=>1,'EditView'=>1,'PriceBookEditView'=>1,'QuickCreate'=>1,'VendorEditView'=>1,'Delete'=>2,'DeletePriceBook'=>2,'DeleteVendor'=>2,'index'=>3,'Popup'=>3,'DetailView'=>4,'PriceBookDetailView'=>4,'TagCloud'=>4,'VendorDetailView'=>4,'Import'=>5,'Export'=>6,'Merge'=>8,'ConvertLead'=>9,'DuplicatesHandling'=>10);
+
+$action_name_array=array(0=>'Save',1=>'EditView',2=>'Delete',3=>'index',4=>'DetailView',5=>'Import',6=>'Export',8=>'Merge',9=>'ConvertLead',10=>'DuplicatesHandling');
+?>
